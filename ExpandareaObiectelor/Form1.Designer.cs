39b132c61393434c26b786955d1ebb0ff247a5e7
proda <prodaniucandrei@gmail.com>
2017-10-19 12:56:20 +0300

    Initial commit

---
+++ ExpandareaObiectelor/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ExpandareaObiectelor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.panel = new System.Windows.Forms.Panel();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.expandToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(991, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // panel
+            // 
+            this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel.Location = new System.Drawing.Point(0, 24);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(991, 480);
+            this.panel.TabIndex = 1;
+            this.panel.DoubleClick += new System.EventHandler(this.panel_DoubleClick);
+            this.panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_MouseDown);
+            this.panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_MouseMove);
+            this.panel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel_MouseUp);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.expandToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // expandToolStripMenuItem
+            // 
+            this.expandToolStripMenuItem.Name = "expandToolStripMenuItem";
+            this.expandToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.expandToolStripMenuItem.Text = "Expand";
+            this.expandToolStripMenuItem.Click += new System.EventHandler(this.expandToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 504);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.ToolStripMenuItem expandToolStripMenuItem;
+    }
+}
+
+
---
+++ ExpandareaObiectelor/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpandareaObiectelor
+{
+    public partial class Form1 : Form
+    {
+        Graphics gp;
+        List<Point> points;
+        Point _previousPoint;
+        Pen pen;
+        bool draw;
+        public Form1()
+        {
+            points = new List<Point>();
+            InitializeComponent();
+            gp = panel.CreateGraphics();
+            pen = new Pen(Color.Black);
+            _previousPoint = new Point();
+        }
+
+        private void panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            Point p = new Point(e.X, e.Y);
+            if (_previousPoint.X > 0 && _previousPoint.Y > 0)
+            {
+
+                gp.DrawLine(pen, _previousPoint.X, _previousPoint.Y, e.X, e.Y);
+            }
+
+            _previousPoint.X = e.X;
+            _previousPoint.Y = e.Y;
+            points.Add(_previousPoint);
+
+        }
+
+        private void panel_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void panel_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void panel_DoubleClick(object sender, EventArgs e)
+        {
+            draw = false;
+        }
+
+        private void expandToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //pen.Width = 20;
+            var path = new GraphicsPath();
+
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                //gp.DrawLine(pen, points[i], points[i+1]);
+                path.StartFigure();
+                path.AddLine(points[i], points[i + 1]);
+                if (i == points.Count - 2)
+                {
+                    path.AddLine(points[i+1], points[0]);
+                    path.AddCurve(new Point[] { points[i], points[i + 1], points[0] });
+                }
+            }
+            pen.Color = Color.Red;
+
+            gp.DrawPath(pen, path);
+
+
+
+        }
+    }
+}
